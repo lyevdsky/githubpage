ed0b6db81aae94c80fdacfbea1accd6156fb668f
hyevd <2228470551@qq.com>
2018-12-06 15:24:57 +0800

    bbb

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About2</router-link>
+      <router-link to="/about">About2bbb</router-link>
     </div>
     <router-view/>
   </div>
