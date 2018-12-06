743703892676b66586492c786b5181fb7d081e80
hyevd <631441457@qq.com>
2018-12-06 15:32:44 +0800

    Dev (#2)
    * .
    * aaa

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About2</router-link>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About2bbb</router-link>
     </div>
     <router-view/>
   </div>
